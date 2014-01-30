5f979b4a2a0cbc3dcacbf110cbcc96a61bda55cb
pboyd <peter.g.boyd@gmail.com>
2014-01-30 13:55:33 -0500

    added two more modules from genstruct

---
+++ LinAlg.py
@@ -0,0 +1,48 @@
+import numpy as np
+RAD2DEG = 180./np.pi
+
+class LinAlg(object):
+    """class methods to encompass all the general linear algebra used in the program"""
+
+    @staticmethod
+    def calc_angle(v1, v2):
+        """Returns the angle in radians between vectors 'v1' and 'v2'"""
+        v1_u = v1[:3] / np.linalg.norm(v1[:3])
+        v2_u = v2[:3] / np.linalg.norm(v2[:3])
+        #Note: using the test (v1_u==v2_u).all(), which returns
+        # true if all values of v1_u are equal to v2_u fails here.
+        # there must be a rounding error.
+        if np.allclose(v1_u, v2_u): 
+            return 0.0
+        elif np.allclose(v1_u, -v2_u): 
+            return np.pi 
+        angle = np.arccos(np.dot(v1_u, v2_u))
+        if np.isnan(angle):
+            if np.allclose(v1_u, v2_u):
+                return 0.0
+            else:
+                return np.pi
+        return angle
+
+    @staticmethod
+    def rotation_matrix(axis, angle, point=None):
+        """
+        returns a 3x3 rotation matrix based on the
+        provided axis and angle
+        """
+        axis = np.array(axis)
+        axis = axis / np.linalg.norm(axis)
+        a = np.cos(angle / 2.)
+        b, c, d = -axis*np.sin(angle / 2.)
+
+        R = np.array([[a*a + b*b - c*c - d*d, 2*(b*c - a*d), 2*(b*d + a*c)],
+                  [2*(b*c + a*d), a*a + c*c - b*b - d*d, 2*(c*d - a*b)],
+                  [2*(b*d - a*c), 2*(c*d + a*b), a*a + d*d - b*b - c*c]])
+
+        M = np.identity(4)
+        M[:3,:3] = R
+        if point is not None:
+            # rotation not around origin
+            point = np.array(point[:3], dtype=np.float64, copy=False)
+            M[:3,3] = point - np.dot(R, point)
+        return M

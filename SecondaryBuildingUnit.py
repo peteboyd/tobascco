92f4cc0465258e0eb26fd4a41cae593f3da0df88
pboyd <peter.g.boyd@gmail.com>
2014-05-02 13:49:59 -0400

    added c function

--- SecondaryBuildingUnit.py
+++ SecondaryBuildingUnit.py
@@ -165,7 +165,7 @@ class SBU(object):
         
         # rotate the atoms
         [a.rotate(rotation_matrix) for a in self.atoms]
-    
+   
     def translate(self, v):
         vector = v - self.COM[:3]
         [c.translate(vector) for c in self.connect_points]
